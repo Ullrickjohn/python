b4e9b4dd22d247c114cac6a2d87e94d2d69eff31
John <ullrickjojn@gmail.com>
2023-12-06 16:24:40 +0500

    complated calculato in python

---
+++ python5.py
@@ -0,0 +1,17 @@
+#format ()
+# s = input('Kiriting:')
+# print('Salom {}'.format(s.capitalize()))
+# truncate long string
+# print('{:.5}'.format(s))
+# print(s.capitalize())
+# casefold
+# count
+# print(s.count('dasturlash'))
+# s = 'salam'
+# print(s[1:5])
+
+# teskariga ogirish inedex
+s = input('Matn yozing: ' '')
+print(s[6:11][0:5] .capitalize())
+# print(s[0:5] .capitalize())
+#  hello world
---
+++ pythonpractice.py
@@ -0,0 +1,13 @@
+a = input('ISM:' )
+b = input('Familiya:' )
+
+password = 1111
+c = input('Parolni kiriting:' )
+
+print( a.capitalize() , b.capitalize() , c)
+
+if password == 1111:
+    print("Parol to'gri")
+    
+else:
+    print('Negative number')
